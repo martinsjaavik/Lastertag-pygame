b4dd87a265c46699c5bdc21bf4e044abcdc8886d
martinsjaavik <116551751+martinsjaavik@users.noreply.github.com>
2023-11-06 11:56:35 +0100

    Add files via upload

---
+++ pygame/sdf.py
@@ -0,0 +1,15 @@
+
+
+def my_func(x, toms=True):
+
+    if not type(x) == int:
+        print( "feil")
+    
+    elif toms == False:
+        print("Gjør greie")
+
+    else:
+        print("Gjør andre greie")
+
+    
+my_func(5)
